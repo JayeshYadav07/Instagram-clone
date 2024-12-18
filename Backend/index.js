951468b3f453ef4aa8084d97d290a96e62807920
JayeshYadav07 <jayeshyadav8080@gmail.com>
2024-12-18 23:16:05 +0530

    basic middleware added

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+// Middlewares
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL || "http://localhost:5173/",
+		credentials: true,
+	})
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json());
 
+// Routes
 app.get("/", (req, res) => {
 	res.status(200).send("Welcome to Instagram-clone API");
 });
 
-app.listen(8000, () => {
-	console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
